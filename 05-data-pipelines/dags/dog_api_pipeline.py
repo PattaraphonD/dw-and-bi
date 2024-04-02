9109c982fff1343c41cc9292fd40c25a70e64606
Pattaraphon D. <134765051+PattaraphonD@users.noreply.github.com>
2024-04-02 15:19:31 +0000

    update dog_spi file

--- 05-data-pipelines/dags/dog_api_pipeline.py
+++ 05-data-pipelines/dags/dog_api_pipeline.py
@@ -4,20 +4,23 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-from 
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.utils import timezone
 
+import requests
+import json
+
 def _get_dog_api():
-    response = request.get("https://dog.ceo/api/breeds/image/random")
+    response = requests.get("https://dog.ceo/api/breeds/image/random")
     data = response.json()
     logging.info(data)
-    with open("/opt/airflow/dags/dog.json")
-
+    with open("/opt/airflow/dags/dog.json", "w") as f: # write python file // for append use "a" but need to assign to start new line
+        json.dump(data, f)
 
 with DAG(
     "dog_api_pipeline", #dag_id should be same as filename
     start_date=timezone.datetime(2024, 3, 23),
-    schedule="@dairy",
+    schedule="@daily", # Cron Expression
     tags=["DS525"],
 ):
     start = EmptyOperator(task_id="start")
@@ -29,9 +32,13 @@ with DAG(
 
     upload_to_gcs = LocalFilesystemToGCSOperator(
         task_id="upload_to_gcs",
-        src=""
+        src="/opt/airflow/dags/dog.json",
+        dst="dog.json",
+        bucket="swu-ds525-8888",
+        gcp_conn_id="my_gcp_conn"
+        #move_object=True
     )
 
     end = EmptyOperator(task_id="end")
 
-    start >> get_dog_api >> end
+    start >> get_dog_api >> upload_to_gcs >> end
